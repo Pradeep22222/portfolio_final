4986ebe9388fe33039f4d378f5a5109de65f5c8f
PK <pradeepdhital124@gmail.com>
2023-02-26 13:13:55 +1100

    all description added on the projects

--- frontend/src/components/projects/projectsData.js
+++ frontend/src/components/projects/projectsData.js
@@ -30,6 +30,8 @@ export const projectsData = [
     title: "React Prank Calculator",
     codeLink: "https://github.com/bishalk21/react-prank-calculator",
     liveLink: "https://react-prank-calculator-sigma.vercel.app/",
+    content:
+      "Main tasks are state handling, calling API, storing and accessing data, error hadnling etc.",
   },
   {
     id: 3,
@@ -38,6 +40,8 @@ export const projectsData = [
     title: "My Movie List",
     codeLink: "https://github.com/bishalk21/movie-app",
     liveLink: "https://movie-app-bishalk21.vercel.app/",
+    content:
+      "Main tasks are state handling, calling API, storing and accessing data, error handling etc, setting up categories, switching categories, grid and list view etc.",
   },
   {
     id: 4,
@@ -46,6 +50,8 @@ export const projectsData = [
     title: "My To Do List App",
     codeLink: "https://github.com/bishalk21/FS-not-to-do-app",
     liveLink: "https://not-to-do-list-app.herokuapp.com/",
+    content:
+      "Main task was using learning JavaScript functions well, handling functions and data flow.",
   },
   {
     id: 5,
@@ -54,6 +60,8 @@ export const projectsData = [
     title: "My Finance Tracker",
     codeLink: "https://github.com/bishalk21/my-finance-tracker",
     liveLink: "https://my-finance-tracker.herokuapp.com/",
+    content:
+      "It is a real life project I developed for Down Syndrome Queensland. It was a part of the curriculum that we develop a programme for a real client upon their request and necessities. It has already been handed to the client but I am still voluntarily working to make it better. The main feature applied was event booking with the help of the calendar. The organization organizes different events on different dates, and the events were supposed to have different individual links and those links to be inserted on the calendar on the specific day the event is on. The languages used are HTML, CSS and JavaScript. Sass was used for the CSS compilation.",
   },
 
   {
@@ -63,5 +71,17 @@ export const projectsData = [
     title: "Habit Tracker",
     codeLink: "https://github.com/bishalk21/habit-tracker-system",
     liveLink: "https://bishalk21.github.io/habit-tracker-system/",
+    content:
+      "Is was a simple website I built for one of my subject, Human Computer Interaction in Bachelor Degree. It was built by just Html, CSS and I used Saas for CSS compilation. On this project I got a chance to learn HTML and CSS in much more details.",
+  },
+  {
+    id: 7,
+    image: habitTracker,
+    transitionImage: habitTracker,
+    title: "Habit Tracker",
+    codeLink: "https://github.com/bishalk21/habit-tracker-system",
+    liveLink: "https://bishalk21.github.io/habit-tracker-system/",
+    content:
+      "It is a full stack multiple user react app built using NodeJs with expressJs on backend and MongoDB with Mongoose for the database.",
   },
 ];
