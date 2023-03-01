f88a387405ac942b38c2ec0b286dd571b47e1298
PK <pradeepdhital124@gmail.com>
2023-03-01 11:52:33 +1100

    general message form is designed

--- frontend/src/components/Footer/ContactForm.js
+++ frontend/src/components/Footer/ContactForm.js
@@ -1,32 +1,79 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 export const ContactForm = () => {
   return (
     <div>
-      <Form>
+      <Form className="mt-3">
         <Form.Text className="text-muted">Your details are kept safe</Form.Text>
-        <Form.Group className="mb-3" controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Leave your name here please" />
-        </Form.Group>
+        <Row>
+          <Col sm={2}></Col>
+          <Col sm={4}>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label> First Name</Form.Label>
+              <Form.Control
+                className="text-center"
+                type="text"
+                placeholder="First Name"
+              />
+            </Form.Group>
+          </Col>
 
-        <Form.Group className="mb-3" controlId="formBasicContact">
-          <Form.Label>Contact Details</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Leave your contact number or email here please"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicMessage">
-          <Form.Label>Message</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Leave your message here please"
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          send
-        </Button>
+          <Col sm={4}>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label> Last Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Last Name"
+                className="text-center"
+              />
+            </Form.Group>
+          </Col>
+          <Col sm={2}></Col>
+          <Col sm={2}></Col>
+          <Col sm={4}>
+            <Form.Group className="mb-3" controlId="formBasicContact">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                className="text-center"
+                type="text"
+                placeholder="example@mail.com"
+              />
+            </Form.Group>
+          </Col>
+          <Col sm={4}>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control
+                className="text-center"
+                type="text"
+                placeholder="Phone Number"
+              />
+            </Form.Group>
+          </Col>
+          <Col sm={2}></Col>
+          <Col sm={2}></Col>
+          <Col sm={8}>
+            <Form.Group className="mb-3" controlId="formBasicMessage">
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                className="text-center"
+                as="textarea"
+                placeholder="Leave your message here please"
+                rows={3}
+              />
+            </Form.Group>
+          </Col>
+          <Col sm={2}></Col>
+          <Col sm={2}></Col>
+          <Col sm={8}>
+            <Button id="message_button" variant="none">
+              Send
+            </Button>
+          </Col>
+          <Col sm={2}></Col>
+        </Row>
       </Form>
     </div>
   );
