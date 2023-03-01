4246342843a54e1fa170fceffe9ef511f72b20f7
PK <pradeepdhital124@gmail.com>
2023-03-01 21:24:58 +1100

    backend server is being done' git status

--- backend/src/model/message/messageModel.js
+++ backend/src/model/message/messageModel.js
@@ -1,5 +1,13 @@
-import messageSchema from "./messageSchem.js"
+import messageSchema from "./messageSchema.js"
 
 export const sendMessage = (obj) => {
     return messageSchema(obj).save()
+}
+
+export const getMessages = () => {
+    return messageSchema.find();
+}
+
+export const deleteMessage = (_id) => {
+    return messageSchema.findByIdAndDelete(_id);
 }
--- backend/src/routers/MessageRouter.js
+++ backend/src/routers/MessageRouter.js
@@ -1,30 +1,39 @@
 import express from "express";
-import { sendMessage } from "../model/message/messageModel.js";
+import {
+  deleteMessage,
+  getMessages,
+  sendMessage,
+} from "../model/message/messageModel.js";
 const router = express.Router();
 
-router.get("/message", (req, res, next) =>{
-    try {
-        res.json({
-            status: "success",
-            message:"response from get method , message router"
-})        
-    } catch (error) {
-        next(error)
-    }
-})
-router.post("/message", async(req, res, next) => {
+router.get("/message", async (req, res, next) => {
   try {
-    const result = await sendMessage(req.body);
+    const result = await getMessages();
     res.json({
       status: "success",
-      message: "response from post method , message router",
+      message: "response from get method , message router",
+      result,
     });
   } catch (error) {
     next(error);
   }
 });
-router.delete("/message", (req, res, next) => {
+router.post("/message", async (req, res, next) => {
   try {
+    const result = await sendMessage(req.body);
+    res.json({
+      status: "success",
+      message: "response from post method , message router",
+      result,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+router.delete("/message", async(req, res, next) => {
+  try {
+    const { _id } = req.body;
+    const result = await deleteMessage(_id);
     res.json({
       status: "success",
       message: "response from delete method , message router",
